809a003d454ae0510123c74771f855b3599fc62b
Volodymyr Voloshyn <mailvoloshyn@gmail.com>
2025-07-11 18:24:49 +0300

    choice for credentials

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -6,10 +6,12 @@ import GaragePage from "../pom/pages/GaragePage";
 const authFile = ".auth/user.json";
 
 setup("Authenticate user with saving storage state", async ({ browser }) => {
+  console.log("USERNAME:", process.env.HTTP_CREDENTIALS_USERNAME);
+console.log("PASSWORD:", process.env.HTTP_CREDENTIALS_PASSWORD);
   const context = await browser.newContext({
     httpCredentials: {
-      username: process.env.HTTP_CREDENTIALS_USERNAME!,
-      password: process.env.HTTP_CREDENTIALS_PASSWORD!,
+      username: process.env.HTTP_CREDENTIALS_USERNAME! || 'guest',
+      password: process.env.HTTP_CREDENTIALS_PASSWORD! || 'welcome2qauto',
     },
   });
   const page = await context.newPage();
